b56dd05de86f770cf9b99cb306f13e000402df89
Eduardo Barros <edu.barros101@gmail.com>
2022-10-13 12:25:32 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Modal Usando a Tag dialog
+
+Este projeto é um exemplo de uso da tag dialog, que é útil para criar modais, janelas de diálogo.
+
+## Referência
+
+Canal do Mayk Brito: https://www.youtube.com/watch?v=hg66uftLsTQ&ab_channel=MaykBrito
---
+++ scripts.js
@@ -0,0 +1,11 @@
+const button = document.querySelector('button')
+const modal = document.querySelector('dialog')
+const buttonClose = document.querySelector('.close')
+
+button.onclick = function () {
+  modal.showModal()
+}
+
+buttonClose.onclick = function () {
+  modal.close()
+}
